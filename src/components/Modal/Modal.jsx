a0a426c59aee277d13a9937bd6e3dfb0d33ac73d
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-06-30 22:52:09 +0300

    Fixed modal size

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -16,7 +16,6 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
@@ -65,7 +64,8 @@ export const EditModal = ({ isOpen, id, name, number, onClose }) => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
+                <Box sx={{ ...style, width: { xs: 240, sm: 400 } }}
+                >
                     <Box component="form"
                     autoComplete="off"
                     noValidate
